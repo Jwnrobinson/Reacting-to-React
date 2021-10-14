46804a6405000365370a675c134dcbc1e7dafef5
Jwnrobinson <89938078+Jwnrobinson@users.noreply.github.com>
2021-10-14 09:56:12 -0500

    Add files via upload

---
+++ src/components/Greeter2.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const Greet2 = props => {
+    return (
+        <div>
+        <h1>
+            Ugh {props.phrase},My name is {props.name}.
+            
+        </h1>
+
+        </div>
+    );
+};
+export default Greet2;
---
+++ src/components/Greeter3.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const Greet3 = props => {
+    return (
+        <div>
+        <h1>
+            HiHo {props.phrase}, My name is {props.name}.
+            
+        </h1>
+
+        </div>
+    );
+};
+export default Greet3;
